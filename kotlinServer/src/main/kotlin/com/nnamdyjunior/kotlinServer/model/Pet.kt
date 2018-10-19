05e1243f33a0e465ecb99d98dcaca5863a6a761a
nnamdyjunior <nnamdyjunior@Nnamdis-MacBook-Pro.local>
2018-10-18 22:03:09 -0600

    deleted stuff

--- kotlinServer/src/main/kotlin/com/nnamdyjunior/kotlinServer/model/Pet.kt
+++
@@ -1,9 +0,0 @@
-package com.nnamdyjunior.kotlinServer.model
-
-import javax.persistence.Entity
-import javax.persistence.Id
-
-@Entity
-data class Pet(@Id val name:String,
-               val type: String,
-               val age: Int)
